320be128df409285387a5602ecfb0155374b971e
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-14 15:24:21 -0500

    Add: Query Provider

---
+++ src/components/QueryProvider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { useState } from 'react'
+
+interface Props {
+    children: React.ReactNode
+}
+export const QueryProvider = ({children} : Props) => {
+    // Create a client
+    const [queryClient] = useState(() => new QueryClient())
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+    </QueryClientProvider>
+  )
+}
