cece3fc378d54af12b5b7060182c5d1e2c958f0a
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-02 17:32:28 -0500

    Change:  Index pae Calender

--- src/app/(admin)/(others-pages)/mapas/page.tsx
+++ src/app/(admin)/(others-pages)/mapas/page.tsx
@@ -1,19 +1,13 @@
-import Calendar from "@/components/calendar/Calendar";
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import { Metadata } from "next";
-import React from "react";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb"
+import { AccidentsByDateMap } from "@/components/maps/AccidentsByDateMap"
 
-export const metadata: Metadata = {
-  title: "Next.js Calender | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
-  // other metadata
-};
-export default function page() {
+export default function Maps() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Calendar" />
-      <Calendar />
+      <PageBreadcrumb pageTitle="Mapas" />
+      <div className="space-y-5 sm:space-y-6">
+        <AccidentsByDateMap />
+      </div>
     </div>
-  );
+  )
 }
