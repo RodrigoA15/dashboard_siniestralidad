1bb6b652c37e7b592c774adcac0ad29cd18679df
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-02 17:31:14 -0500

    Fix: Folder Initial map

--- src/components/InitialMaps/InitialMap.tsx
+++ src/components/InitialMaps/InitialMap.tsx
@@ -4,15 +4,15 @@ import { useQuery } from '@tanstack/react-query';
 import { useFetchMaps } from '@/api/Maps/fetchMaps';
 
 interface Props {
-  coordenadaX : number;
-  coordenadaY : number;
+  coordenadaX: number;
+  coordenadaY: number;
 }
 export const InitialMap = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const {accidentsByDate} = useFetchMaps()
-  const {data, isLoading} = useQuery({
-    queryKey: ['accidentsByDate', new Date('2024-11-04'), new Date('2024-11-14'), 'h'],
-    queryFn: () => accidentsByDate(new Date('2024-11-04'), new Date('2024-11-14'), 'h')
+  const { accidentsByDate } = useFetchMaps()
+  const { data, isLoading } = useQuery({
+    queryKey: ['accidentsByDate', new Date('2025-03-10'), new Date('2025-03-12'), 'h'],
+    queryFn: () => accidentsByDate('2025-03-10', '2025-03-12', 'h')
   })
 
   useEffect(() => {
@@ -21,14 +21,16 @@ export const InitialMap = () => {
     if (mapContainer.current) {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/dark-v11",
+        style: "mapbox://styles/mapbox/streets-v12",
         center: [-76.614, 2.441],
         zoom: 12,
-        maxZoom: 15,
+        pitch: 45,
+        bearing: -10.4,
+        antialias: true,
       });
-
+      const fullScreen = new mapboxgl.FullscreenControl()
       // Add zoom controls
-      map.addControl(new mapboxgl.FullscreenControl(), "top-left");
+      map.addControl(fullScreen, "top-left");
       map.addControl(new mapboxgl.NavigationControl(), "top-left");
 
       data.map((item: Props) => {
@@ -39,10 +41,10 @@ export const InitialMap = () => {
 
       // Clean up on unmount
       return () => map.remove();
-    } 
+    }
   }, [data]);
 
-  if(isLoading) return <p>Loading Map...</p>
+  if (isLoading) return <p>Loading Map...</p>
 
-   return <div id="map" ref={mapContainer} style={{ height: '100%' }}></div>;
+  return <div id="map" ref={mapContainer} style={{ height: '100%' }}></div>;
 }
