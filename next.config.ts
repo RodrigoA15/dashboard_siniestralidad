8371a0acaa6013afa2ddcffdd1ba6fbd1ebb129e
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-11 16:06:15 -0500

    Add: Context app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  basePath: "/siniestralidad",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
