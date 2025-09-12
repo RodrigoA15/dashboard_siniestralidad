0e5b8c5a5d332bf43ef5425b135b47cc60f50814
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-12 09:57:24 -0500

    Fix: Center inside cards

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -3,39 +3,39 @@ import React from "react";
 import { useQueries } from "@tanstack/react-query";
 import { useFetchTotals } from "@/api/dashboard/fetchTotals";
 import { useYear } from '../../context/YearContext';
-export const fetchTotalSiniestros = async() =>  {
+export const fetchTotalSiniestros = async () => {
 }
 export const EcommerceMetrics = () => {
   const { fetchTotals, fetchTotalYears } = useFetchTotals();
-  const {year} = useYear();
+  const { year } = useYear();
   const [totals, totalYears] = useQueries({
-  queries : [
-  { 
-    queryKey: ["totals"],
-    queryFn: fetchTotals,
-    placeholderData: [{
-      total_accidents: 0,
-    }]
-  },
-  { 
-    queryKey: ["totalYears", year],
-    queryFn: () => fetchTotalYears(year),
-    placeholderData: [{
-      TOTAL: 0,
-    }]
-  }
-  ]
+    queries: [
+      {
+        queryKey: ["totals"],
+        queryFn: fetchTotals,
+        placeholderData: [{
+          total_accidents: 0,
+        }]
+      },
+      {
+        queryKey: ["totalYears", year],
+        queryFn: () => fetchTotalYears(year),
+        placeholderData: [{
+          TOTAL: 0,
+        }]
+      }
+    ]
   });
 
-  if(totals.isError){
+  if (totals.isError) {
     return <div>Error</div>;
   }
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
-      {/* <!-- Metric Item Start --> */}
+      {/* */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-end justify-between">
+        <div className="flex flex-col items-center justify-center text-center">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Total siniestros
@@ -46,11 +46,8 @@ export const EcommerceMetrics = () => {
           </div>
         </div>
       </div>
-      {/* <!-- Metric Item End --> */}
-
-      {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-        <div className="flex items-end justify-between">
+        <div className="flex flex-col items-center justify-center text-center">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Total siniestros {year}
@@ -61,7 +58,6 @@ export const EcommerceMetrics = () => {
           </div>
         </div>
       </div>
-      {/* <!-- Metric Item End --> */}
     </div>
   );
 };
--- src/components/type_service/TypeServicesMetrics.tsx
+++ src/components/type_service/TypeServicesMetrics.tsx
@@ -9,31 +9,30 @@ interface Props {
   TOTAL: number
 }
 export const TypeServicesMetrics = () => {
-const {year} = useYear()
-const {accidentsByTypeServices} = useFetchTypeServices()
-const {data} = useQuery({
-    queryKey: ["accidents-type-services", year], 
-    queryFn: () => accidentsByTypeServices(year), 
+  const { year } = useYear()
+  const { accidentsByTypeServices } = useFetchTypeServices()
+  const { data } = useQuery({
+    queryKey: ["accidents-type-services", year],
+    queryFn: () => accidentsByTypeServices(year),
     placeholderData: [{
       TOTAL: 0,
       DESC_SERVICIO: "Cargando..."
-    }]})
+    }]
+  })
   return (
     <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 xl:grid-cols-6'>
-  {data.map((item: Props) => (
-      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]" key={item.DESC_SERVICIO}>
-        <div className="flex items-end justify-between">
-          <div>
+      {data.map((item: Props) => (
+        <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]" key={item.DESC_SERVICIO}>
+          <div className="flex flex-col items-center justify-center text-center">
             <span className="text-sm text-gray-500 dark:text-gray-400">
-              {item.DESC_SERVICIO || "Sin descripción"}
+              {item.DESC_SERVICIO || "Sin descripción"}
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
               {item.TOTAL}
             </h4>
           </div>
         </div>
-      </div>
-  ))}
+      ))}
     </div>
   )
 }
